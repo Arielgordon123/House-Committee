18d105ffd0942860701b5afc540490e794d7159e
Ariel Gordon <arielgordon123@gmai.com>
2019-05-24 02:06:09 +0300

    add Array List in order to stoe Payments

--- Tenant.java
+++ Tenant.java
@@ -1,10 +1,22 @@
 package House_Committee;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Tenant extends Person {
 
+    private String apartmentNumber;
+    private double monthlyPayment;
+    private ArrayList<HashMap<String ,String>> paymentsArr;
 
     public Tenant(String firstName, String lastName, String id, String userName, String hashedPassword) {
         // call to main Person Object Constractor
         super(firstName, lastName, id, userName, hashedPassword);
+    }
+
+
+    public int getAllPayment() {
+
+        return 0;
     }
 }
